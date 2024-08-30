518af39796f8c3d25af1e5c5d9315781ad6d375b
Vitor :) <vitor.azevedoo2812@gmail.com>
2024-08-30 14:12:45 -0300

    Add files via upload

---
+++ Projeto/src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+import './NavBar.css';
+
+function NavBar() {
+
+  return(
+    
+    <>
+    <nav>
+      <h1>Therapy</h1>
+      <div>
+        <a>Home</a>
+        <a>Serviços</a>
+        <a>Sobre Nós</a>
+      </div>
+
+      <button>Entrar</button>
+    </nav>
+    </>
+  );
+}
+
+export default NavBar;
