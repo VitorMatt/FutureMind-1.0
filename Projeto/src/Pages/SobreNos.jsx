8ca6deaaff2d45baf5aa6e260aa3c772ce544963
EVELYN LOHANNY SANTOS DA SILVA <evelyn_l_silva@estudante.sesisenai.org.br>
2024-10-02 17:14:28 -0300

    Desenvolvimento da SA

--- Projeto/src/Pages/SobreNos.jsx
+++ Projeto/src/Pages/SobreNos.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import './CSS/SobreNos.css'
 import img from '../assets/img.png'
 import Bola from '../assets/Bola.png'
+import NavBar from '../components/NavBar.jsx'
+import Arvore from '../assets/Arvore.png'
 
 function SobreNos() {
   return (
     <div>
+      <NavBar />
         <div className='corpo'>
             <img className='img' src={img}/>
             <div className='infoMissao'>
@@ -15,6 +18,14 @@ function SobreNos() {
         </div>
       <img src={Bola} className='bolinhas'/>
       
+        <div className='infoFundacao'>
+          <div>
+            <h1 className='titulo2'>Fundação:</h1>
+            <p className='texto'>A FutureMind  é uma plataforma que oferece acesso acessível e de qualidade a sessões de psicanálise. Ela conecta indivíduos que buscam apoio psicológico a recém-formados em psicologia, permitindo que esses profissionais em início de carreira ganham experiência prática enquanto cumprem os requisitos de estágio, sob supervisão de psicólogos experientes.</p>
+          </div>
+        </div>
+        <img src={Arvore} className='arvore'/>
+
     </div>
   )
 }
