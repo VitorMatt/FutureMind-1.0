fbc978be7ea7e8f7c860911545e796d3fec6c844
BRENDA MAGANELI CASIMIRO <brenda_casimiro@estudante.sesisenai.org.br>
2024-10-11 17:11:41 -0300

    Cadastro

--- FutureMind/src/pages/EscolhaCadastro.jsx
+++ FutureMind/src/pages/EscolhaCadastro.jsx
@@ -6,12 +6,15 @@ function EscolhaCadastro() {
       <div className='Lado-esquerdo'>
         <h3 className='titulo-Cadastro'>Selecione um tipo de cadastro:</h3>
         <div className='checkboxs'>
-            <div>
+            <div className='estilização-Check'>
            <input type="radio" name="a" id="" className='check1' /><label htmlFor="" className='label1'>Sou Profissional</label>
             </div>
-            <div>
+            <div className='estilização-Check'>
             <input type="radio" name="a" id="" className='check1'/><label htmlFor="" className='label1'>Sou Paciente</label>
             </div>
+        <div className='Proximo'>
+            <button className='proximo-estilizado'>Próximo</button>
+        </div>
         </div>
       </div>
       <div className='lado-Direito'>
