19323e650104359369c78846c6ee2ef73f59696f
MATEUS DA SILVA <mateus_silva44@estudante.sesisenai.org.br>
2024-10-17 17:19:46 -0300

    novas ativ

---
+++ FutureMind/src/pages/Termos.jsx
@@ -0,0 +1,16 @@
+import Termos from './CSS/Termos.css'
+import NavBar from '../components/Navbar.jsx'
+
+export class Termos extends Component {
+  render() {
+    return (
+      <div>
+       <NavBar />
+       oal
+    
+      </div>
+    )
+  }
+}
+
+export default Termos
