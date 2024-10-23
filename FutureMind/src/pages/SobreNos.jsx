39658dfc175ea7ec87c9eebb3fae434a89b1acc4
EVELYN LOHANNY SANTOS DA SILVA <evelyn_l_silva@estudante.sesisenai.org.br>
2024-10-23 17:20:37 -0300

    alterações nas telas

--- FutureMind/src/pages/SobreNos.jsx
+++ FutureMind/src/pages/SobreNos.jsx
@@ -5,21 +5,21 @@ function SobreNos() {
   return (
     <div className='sobreNos-container'>
       <NavBar />
-      <div className='corpo'>
-            <img className='img' src='img.png'/>
-            <div className='infoMissao'>
-                 <h1 className='titulo'>Missão:</h1>
-                <p className='texto'>A FutureMind é uma plataforma que oferece acesso acessível e de qualidade a sessões de psicanálise. Ela conecta indivíduos que buscam apoio psicológico a recém-formados em psicologia, permitindo que esses profissionais em início de carreira ganham experiência prática enquanto cumprem os requisitos de estágio, sob supervisão de psicólogos experientes.</p>
-            </div>
+
+      <div className='conteudoo'>
+        <img src="img.png" className='img'/>
+
+        <div className='descricaoMissao'>
+          <h1>Missão:</h1>
+          <p>A FutureMind é uma plataforma que oferece acesso acessível e de qualidade a sessões de psicanálise. Ela conecta indivíduos que buscam apoio psicológico a recém-formados 
+             em psicologia, permitindo que esses profissionais em início de carreira ganham experiência prática enquanto cumprem os requisitos de estágio, sob supervisão de psicólogos
+             experientes.
+          </p>
         </div>
-      <img src='Bola.png' className='bolinhas'/>      
-        <div className='infoFundacao'>
-          <div>
-            <h1 className='titulo2'>Fundação:</h1>
-            <p className='texto'>A FutureMind é uma plataforma que oferece acesso acessível e de qualidade a sessões de psicanálise. Ela conecta indivíduos que buscam apoio psicológico a recém-formados em psicologia, permitindo que esses profissionais em início de carreira ganham experiência prática enquanto cumprem os requisitos de estágio, sob supervisão de psicólogos experientes.</p>
-          </div>
-        </div>
-        <img src='Arvore.png' className='arvore'/>
+      </div>
+
+      <img src="" alt="" />
+
     </div>
   )
 }
