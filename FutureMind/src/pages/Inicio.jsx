a6a8af652fa87785d47c754598aa41fcf6f675b0
VitorMatt <vitor.azevedoo2812@gmail.com>
2024-10-28 17:19:55 -0300

    Início

--- FutureMind/src/pages/Inicio.jsx
+++ FutureMind/src/pages/Inicio.jsx
@@ -1,5 +1,14 @@
 import Navbar from '../components/Navbar'
 import './CSS/Inicio.css'
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 function Inicio() {
   return (
@@ -71,33 +80,74 @@ function Inicio() {
 
   
       <div className='profissionais'>
-        <h1>PROOOFISSIONAIS</h1>
-        
-        
+        <div className='titulo-inicio'>
+            <h1>
+              Início
+            </h1>
+          </div>
+          <div style={{width: '100%'}}>
+
+          <Swiper
+                // instalar módulos do Swiper
+                modules={[Navigation, Pagination, A11y]}
+                spaceBetween={50}
+                slidesPerView={1}
+                navigation
+                pagination={{ clickable: true }}
+                onSwiper={(swiper) => console.log(swiper)}
+                onSlideChange={() => console.log('slide change')}
+            >
+
+        <SwiperSlide>
+
         <div className='profissional'>
           <h1>PROFISSIONAL 1</h1>
           
         </div>
+        </SwiperSlide>
+        <SwiperSlide>
+
+        <div className='profissional'>
+          <h1>PROFISSIONAL 1</h1>
+          
+        </div>
+        </SwiperSlide>
+      </Swiper>
+        </div>
         
-        <div>
-          <div>
-<h1>
-  aq
-</h1>
+  
+          <div className='titulos'>
+            <h1>
+              Adultos
+            </h1>
           </div>
         <div className='profissional'>
           <h1>PROFISSIONAL 2</h1>
         </div>
-        </div>
 
+        <div className='titulos'>
+            <h1>
+              Adultos
+            </h1>
+          </div>
         <div className='profissional'>
           <h1>PROFISSIONAL 3</h1>
         </div>
 
+        <div className='titulos'>
+            <h1>
+              Adultos
+            </h1>
+          </div>
         <div className='profissional'>
           <h1>PROFISSIONAL 4</h1>
         </div>
 
+        <div className='titulos'>
+            <h1>
+              Adultos
+            </h1>
+          </div>
         <div className='profissional'>
           <h1>PROFISSIONAL 5</h1>
         </div>
