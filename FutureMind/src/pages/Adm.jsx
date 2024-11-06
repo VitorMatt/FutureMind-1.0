25c5be9ff6279de16e3ee264bfb65bd0afb77daf
MateusdasilvaDES <mateus_silva44@estudante.sesisenai.org.br>
2024-11-06 17:09:14 -0300

    novas

--- FutureMind/src/pages/Adm.jsx
+++ FutureMind/src/pages/Adm.jsx
@@ -20,21 +20,44 @@ function Adm() {
         {nome: 'Renato Garcia', instagram: '@RenatoGarcia' , idade: 36, descrição: 'Ola eu sou o Renato', atende: 'Adolescentes, Idosos, Crianças.', especialização: 'Bullying, Autoaceitação'},
         {nome: 'Pedro Henrique', instagram: '@PedroHenrique' , idade: 27, descrição: 'Ola eu sou o Pedro', atende: 'Adolesentes, PCDs, Idosos.', especialização: 'Depressão, Adolescência'},
         {nome: 'Luiza Souza', instagram: '@LuizaSousa' , idade: 23, descrição: 'Ola eu sou a Luiza', atende: 'Pré-Adolescentes, Crianças, Adolecentes.', especialização: 'Angústia, Relacionamentos'},
-        
+        {nome: 'Luiza Souza', instagram: '@LuizaSousa' , idade: 23, descrição: 'Ola eu sou a Luiza', atende: 'Pré-Adolescentes, Crianças, Adolecentes.', especialização: 'Angústia, Relacionamentos'},
+        {nome: 'Luiza Souza', instagram: '@LuizaSousa' , idade: 23, descrição: 'Ola eu sou a Luiza', atende: 'Pré-Adolescentes, Crianças, Adolecentes.', especialização: 'Angústia, Relacionamentos'},
+        {nome: 'Luiza Souza', instagram: '@LuizaSousa' , idade: 23, descrição: 'Ola eu sou a Luiza', atende: 'Pré-Adolescentes, Crianças, Adolecentes.', especialização: 'Angústia, Relacionamentos'},
+        {nome: 'Luiza Souza', instagram: '@LuizaSousa' , idade: 23, descrição: 'Ola eu sou a Luiza', atende: 'Pré-Adolescentes, Crianças, Adolecentes.', especialização: 'Angústia, Relacionamentos'},
+        {nome: 'Luiza Souza', instagram: '@LuizaSousa' , idade: 23, descrição: 'Ola eu sou a Luiza', atende: 'Pré-Adolescentes, Crianças, Adolecentes.', especialização: 'Angústia, Relacionamentos'},
+        {nome: 'Luiza Souza', instagram: '@LuizaSousa' , idade: 23, descrição: 'Ola eu sou a Luiza', atende: 'Pré-Adolescentes, Crianças, Adolecentes.', especialização: 'Angústia, Relacionamentos'},
+        {nome: 'Luiza Souza', instagram: '@LuizaSousa' , idade: 23, descrição: 'Ola eu sou a Luiza', atende: 'Pré-Adolescentes, Crianças, Adolecentes.', especialização: 'Angústia, Relacionamentos'},
+      
       ]
 
   return (
     <div className="bory-container">
       <Navbar/>
        <h1 className="h1_Profissionais_adm">Profissionais:</h1>
-       <div className="container_padrao_adm">{
-
+       <div className="container_padrao_adm">
+         <div style={{width: '100%'}}>
+       <Swiper
+                // instalar módulos do Swiper
+                modules={[Navigation, Pagination, A11y]}
+                spaceBetween={1}
+                slidesPerView={2}
+                navigation
+                pagination={{ clickable: true }}
+                onSwiper={(swiper) => console.log(swiper)}
+                onSlideChange={() => console.log('slide change')}
+                className = 'div_s'
+            >
+        {
+        
+         
          Profissionais.map((p, index) => (
 
+        <SwiperSlide>
          <div 
           key={index}
           className="Div_profissionais_adm"
-         >
+          style={{width: "70%", }}
+          >
           <div className="div_um_interna_profissional">
             <img src="Foto_perfil.svg" alt="" />
             <p>{p.instagram}</p>
@@ -48,17 +71,21 @@ function Adm() {
           <div className="div_dois_interna_profissional">
              <h1>{p.nome}</h1> 
              <p className="p_padrao">Eu atendo...</p>
-             <p>{p.atende}</p>
+             <p className="informacao_geral">{p.atende}</p>
              <p className="p_padrao">Especialidade(s):</p>
-             <p>{p.especialização}</p>
+             <p className="informacao_geral">{p.especialização}</p>
           </div>
 
          </div>
+         </SwiperSlide>
 
          ))
         }
+      </Swiper>
+        </div>
        </div>
-       <h1 className="h1_Paciente_adm">Paciente:</h1> 
+       <h1 className="h1_Paciente_adm">Paciente:</h1>
+        
     </div>
   )
 }
