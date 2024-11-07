c3a2ead53a9382a533973cdeaf98401e224aa56b
MateusdasilvaDES <mateus_silva44@estudante.sesisenai.org.br>
2024-11-07 17:22:35 -0300

    alterações feitas

--- FutureMind/src/pages/Adm.jsx
+++ FutureMind/src/pages/Adm.jsx
@@ -15,27 +15,29 @@ function Adm() {
 
       const Profissionais = 
       
-      [
+        [
 
-        {nome: 'Renato Garcia', instagram: '@RenatoGarcia' , idade: 36, descrição: 'Ola eu sou o Renato', atende: 'Adolescentes, Idosos, Crianças.', especialização: 'Bullying, Autoaceitação'},
-        {nome: 'Pedro Henrique', instagram: '@PedroHenrique' , idade: 27, descrição: 'Ola eu sou o Pedro', atende: 'Adolesentes, PCDs, Idosos.', especialização: 'Depressão, Adolescência'},
-        {nome: 'Luiza Souza', instagram: '@LuizaSousa' , idade: 23, descrição: 'Ola eu sou a Luiza', atende: 'Pré-Adolescentes, Crianças, Adolecentes.', especialização: 'Angústia, Relacionamentos'},
-        {nome: 'Luiza Souza', instagram: '@LuizaSousa' , idade: 23, descrição: 'Ola eu sou a Luiza', atende: 'Pré-Adolescentes, Crianças, Adolecentes.', especialização: 'Angústia, Relacionamentos'},
-        {nome: 'Luiza Souza', instagram: '@LuizaSousa' , idade: 23, descrição: 'Ola eu sou a Luiza', atende: 'Pré-Adolescentes, Crianças, Adolecentes.', especialização: 'Angústia, Relacionamentos'},
-        {nome: 'Luiza Souza', instagram: '@LuizaSousa' , idade: 23, descrição: 'Ola eu sou a Luiza', atende: 'Pré-Adolescentes, Crianças, Adolecentes.', especialização: 'Angústia, Relacionamentos'},
-        {nome: 'Luiza Souza', instagram: '@LuizaSousa' , idade: 23, descrição: 'Ola eu sou a Luiza', atende: 'Pré-Adolescentes, Crianças, Adolecentes.', especialização: 'Angústia, Relacionamentos'},
-        {nome: 'Luiza Souza', instagram: '@LuizaSousa' , idade: 23, descrição: 'Ola eu sou a Luiza', atende: 'Pré-Adolescentes, Crianças, Adolecentes.', especialização: 'Angústia, Relacionamentos'},
-        {nome: 'Luiza Souza', instagram: '@LuizaSousa' , idade: 23, descrição: 'Ola eu sou a Luiza', atende: 'Pré-Adolescentes, Crianças, Adolecentes.', especialização: 'Angústia, Relacionamentos'},
-        {nome: 'Luiza Souza', instagram: '@LuizaSousa' , idade: 23, descrição: 'Ola eu sou a Luiza', atende: 'Pré-Adolescentes, Crianças, Adolecentes.', especialização: 'Angústia, Relacionamentos'},
+          {img: 'renato.png' ,nome: 'Renato Garcia', instagram: '@RenatoGarcia' , idade: 36, descrição: 'Ola eu sou o Renato', atende: 'Adolescentes, Idosos, Crianças.', especialização: 'Bullying, Autoaceitação'},
+          {img: 'joao_peedro.png', nome: 'Pedro Henrique', instagram: '@PedroHenrique' , idade: 27, descrição: 'Ola eu sou o Pedro', atende: 'Adolesentes, PCDs, Idosos.', especialização: 'Depressão, Adolescência'},
+          {img: 'jessica.png' ,nome: 'Luiza Souza', instagram: '@LuizaSousa' , idade: 23, descrição: 'Ola eu sou a Luiza', atende: 'Pré-Adolescentes, Crianças, Adolecentes.', especialização: 'Angústia, Relacionamentos'},
+        
+        ]
       
-      ]
+      const Paciente =
+      
+        [
+         
+          {nome: 'Mateus da silva', }
+        ]
+
+
 
   return (
     <div className="bory-container">
       <Navbar/>
        <h1 className="h1_Profissionais_adm">Profissionais:</h1>
        <div className="container_padrao_adm">
-         <div style={{width: '100%'}}>
+         <div style={{width: '100%',}}>
        <Swiper
                 // instalar módulos do Swiper
                 modules={[Navigation, Pagination, A11y]}
@@ -59,8 +61,9 @@ function Adm() {
           style={{width: "70%", }}
           >
           <div className="div_um_interna_profissional">
-            <img src="Foto_perfil.svg" alt="" />
-            <p>{p.instagram}</p>
+
+            <img src={p.img} />
+            <p className="arroba_profissionais">{p.instagram}</p>
             <div className="div_botao_um_adm">
               <button className="botoes_adm">Deletar</button>
             </div>
@@ -75,7 +78,6 @@ function Adm() {
              <p className="p_padrao">Especialidade(s):</p>
              <p className="informacao_geral">{p.especialização}</p>
           </div>
-
          </div>
          </SwiperSlide>
 
@@ -85,7 +87,6 @@ function Adm() {
         </div>
        </div>
        <h1 className="h1_Paciente_adm">Paciente:</h1>
-        
     </div>
   )
 }
