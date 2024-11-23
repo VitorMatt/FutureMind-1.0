3142aba8305a2333ce1df8d34f8c67005de6b7f5
ManassesMarcelino <manassesmarcelino@gmail.com>
2024-11-23 14:31:04 -0300

    salve

--- FutureMind/src/pages/LandingPage.jsx
+++ FutureMind/src/pages/LandingPage.jsx
@@ -62,12 +62,12 @@ function LandingPage() {
           <div className='esquerdo'>
 
             <div className='como-funciona'>
-                 <p>COMO FUNCIONA A TERAPIA ONLINE</p>
+                 <p>Como funciona a terapia online</p>
                 
             </div>
 
             <div className='text-flex'>
-                  <p>A flexibilidade de realizar consultas de qualquer lugar elimina barreiras geográficas, ampliando o acesso à terapia. Lembretes automáticos ajudam a manter a rotina, e ferramentas integradas, como exercícios de atenção plena, potencializam a eficácia do tratamento.</p>
+                  <p>A terapia online é uma forma prática de cuidar da sua saúde mental. Você conhece os profissionais na plataforma, agenda sua consulta no melhor horário e realiza sessões por videochamada com segurança e conforto, sem precisar sair de casa. Uma solução acessível para o seu bem-estar!</p>
             </div>
 
           </div>
@@ -122,10 +122,10 @@ function LandingPage() {
               </div>
               <div className="text-perfil">
                 <div className="nome-idade-perfil">
-                  <h1>Clarice, 54 anos:</h1>
+                  <h1>Carlos, 28 anos:</h1>
                 </div>
                 <div className="relato">
-                  <p>Comecei a terapia online há seis meses e não poderia estar mais feliz com os resultados. A flexibilidade de fazer as sessões de casa me ajudou a abrir o coração e explorar questões que me incomodavam há anos. Sinto que estou finalmente no caminho certo para o autoconhecimento!"</p>
+                  <p>No início, tinha dúvidas sobre a eficácia da terapia online, mas minha experiência foi surpreendente. Meu terapeuta criou um ambiente seguro e acolhedor, onde pude falar abertamente sobre minhas ansiedades. Hoje, me sinto mais confiante </p>
                 </div>
               </div>
             </div>
@@ -136,10 +136,10 @@ function LandingPage() {
               </div>
               <div className="text-perfil">
                 <div className="nome-idade-perfil">
-                  <h1>Clarice, 54 anos:</h1>
+                  <h1>Ana, 45 anos:</h1>
                 </div>
                 <div className="relato">
-                  <p>Comecei a terapia online há seis meses e não poderia estar mais feliz com os resultados. A flexibilidade de fazer as sessões de casa me ajudou a abrir o coração e explorar questões que me incomodavam há anos. Sinto que estou finalmente no caminho certo para o autoconhecimento!"</p>
+                  <p>Ana, 45 anos: "A terapia online mudou minha vida! A abordagem prática e as ferramentas que aprendi nas sessões me ajudaram a lidar melhor com o estresse e cansaço do dia a dia. Sou grata por ter encontrado esse espaço de apoio e compreensão." </p>
                 </div>
               </div>
             </div>
@@ -150,10 +150,10 @@ function LandingPage() {
               </div>
               <div className="text-perfil">
                 <div className="nome-idade-perfil">
-                  <h1>Clarice, 54 anos:</h1>
+                  <h1>Fernando, 37 anos:</h1>
                 </div>
                 <div className="relato">
-                  <p>Comecei a terapia online há seis meses e não poderia estar mais feliz com os resultados. A flexibilidade de fazer as sessões de casa me ajudou a abrir o coração e explorar questões que me incomodavam há anos. Sinto que estou finalmente no caminho certo para o autoconhecimento!"</p>
+                  <p> Fazer terapia online foi uma das melhores decisões da minha vida. O conforto de estar em casa e a conexão que estabeleci com minha terapeuta foram fundamentais para o meu processo de cura. Hoje, me sinto mais leve e em paz comigo mesma.</p>
                 </div>
               </div>
             </div>
