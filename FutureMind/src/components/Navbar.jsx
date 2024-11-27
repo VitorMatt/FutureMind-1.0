3afb9656a844a98fbc1510404757e0f6fd463942
BRENDA MAGANELI CASIMIRO <brenda_casimiro@estudante.sesisenai.org.br>
2024-11-27 15:31:53 -0300

    perfil profissional/paciente

--- FutureMind/src/components/Navbar.jsx
+++ FutureMind/src/components/Navbar.jsx
@@ -1,18 +1,30 @@
 import { useContext, useEffect } from 'react';
-import './CSS/Navbar.css'
-import { Link } from 'react-router-dom'
+import './CSS/Navbar.css';
+import { Link, useNavigate } from 'react-router-dom'; // Adicionando useNavigate
 import { GlobalContext } from '../GlobalContext/GlobalContext';
 
 function Navbar() {
-
   const { user } = useContext(GlobalContext);
-  const userAux = {...user};
+  const navigate = useNavigate(); // Usando o hook de navegação
+  const userAux = { ...user };
 
   useEffect(() => {
-
     userAux.profissional = user.profissional;
     userAux.logado = user.logado;
-  }, [user])
+  }, [user]);
+
+  const handleUserClick = () => {
+    if (userAux.logado) {
+      if (userAux.profissional) {
+        navigate('/perfilprofissional'); // Redireciona para o perfil do profissional
+      } else {
+        navigate('/perfil-paciente'); // Redireciona para o perfil do paciente
+      }
+    } else {
+      navigate('/login'); // Caso o usuário não esteja logado, redireciona para a página de login
+    }
+  };
+
   return (
     <nav className='navbar'>
       <img src='logo (2).png' className='logo-nav'/>
@@ -25,17 +37,12 @@ function Navbar() {
         </div>
 
         {
-          !userAux.logado
-          ?
-          (
-
+          !userAux.logado ? (
             <Link to='/login' className='btn2'>Entrar</Link>
-          )
-          :
-          (
-            <Link to={ userAux.profissional ? '/perfilprofissional' : '/perfil-paciente'} className='btn2'>
-            <img src="iconuser.svg" alt="" className="user" />
-            </Link>
+          ) : (
+            <button onClick={handleUserClick} className='btn2'>
+              <img src="iconuser.svg" alt="" className="user" />
+            </button>
           )
         }
       </div>
